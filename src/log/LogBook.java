cc543dcbb76b3dbda0c61168eeddf00fd902a877
Kevin <stupark@bzz.ch>
2022-05-18 15:42:26 +0200

    Added comments

--- src/log/LogBook.java
+++ src/log/LogBook.java
@@ -3,17 +3,27 @@ package log;
 import java.io.*;
 import java.util.Vector;
 
+/**
+ * Class to manage the Logbook. Is able to Read and Write.
+ * @author Kevin
+ * @since 18.05.2022
+ * @version 1.0
+ *
+ */
 public class LogBook {
-    private Vector<String> entries;
+    private Vector<String> entries = new Vector<>();
     private static LogBook instance;
     private BufferedWriter writer;
     private BufferedReader reader;
-    private boolean fileWritingEnabled = false;
+    private boolean fileWritingEnabled;
 
+    /**
+     * Creates a Logbook. Is private because of Singleton pattern.
+     */
     private LogBook() {
-        File f = new File("logbuch.log");
+        File f = new File("logboook.log");
         try {
-            if (f.createNewFile()==false) {
+            if (!f.createNewFile()) {
                 reader = new BufferedReader(new FileReader(f));
                 readFile();
                 reader.close();
@@ -25,12 +35,22 @@ public class LogBook {
         }
     }
 
+    /**
+     * Creates an instance if not existing.
+     * - Singleton pattern.
+     * @return instance.
+     */
     public static LogBook getLogBookInstance() {
         if (instance == null) instance = new LogBook();
         return instance;
     }
 
-    public void addEntry(String  entry) {
+
+    /**
+     * Adds an entry and saves it to the File.
+     * @param entry to be added.
+     */
+    public void addEntry(String entry) {
         if (fileWritingEnabled) {
             entries.add(entry);
             writeFile(entry);
@@ -38,26 +58,48 @@ public class LogBook {
         }
     }
 
+    /**
+     * Gives the requested entry in form of a String.
+     * @param index of the entry stored.
+     * @return String of the entry.
+     */
     public String getEntry(int index) {
         return entries.get(index);
     }
 
+    /**
+     * Gives back the number of entries stored.
+     * @return int size.
+     */
     public int getSize() {
         return entries.size();
     }
 
+    /**
+     * Closes the writer and deletes the current instance.
+     */
     public void logBookClose() {
         try {
             writer.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        instance = null;
     }
 
+    /**
+     * Prints all logs saved to the console.
+     */
     public void printLog() {
-
+        for (String entry : entries) {
+            System.out.println(entry);
+        }
     }
 
+    /**
+     * writes the entry to a file (appending).
+     * @param entry to be appended.
+     */
     private void writeFile(String entry) {
         try {
             writer.append(entry);
@@ -68,6 +110,10 @@ public class LogBook {
         }
     }
 
+    /**
+     * Reads all entrys in form of stings and stores it in the attribute.
+     * @throws IOException
+     */
     private void readFile() throws IOException{
         while (reader.ready()) {
             entries.add(reader.readLine());
--- src/log/UserAction.java
+++ src/log/UserAction.java
@@ -3,26 +3,46 @@ package log;
 import employees.HRPerson;
 import employees.Person;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * A model class for storing an entry temporarily
+ * @author Kevin
+ * @since 18.05.2022
+ * @version 1.0
+ */
 public class UserAction {
-    public static final int CREACTE_PERSON = 0;
+    public static final int CREATE_PERSON = 0;
     public static final int CHANGE_VALUE = 1;
     public static final int SET_ASSIGNMENT = 2;
     public static final int DELETE_PERSON = 3;
-    private String[] actionDescription;
+    private String[] actionDescription = {"Create Person", "Change Value", "Set Assignment", "Delete Person"};
     private String entry;
 
+    /**
+     * Constructor, generating the entry with the given information.
+     * @param hrPerson doing the action.
+     * @param person affected by the action.
+     * @param action that is being done.
+     */
     public UserAction(HRPerson hrPerson, Person person, int action) {
-        entry = hrPerson.getFirstName();
-        entry += hrPerson.getLastName();
+        entry = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)+": ";
+        entry += hrPerson.getFirstName()+" ";
+        entry += hrPerson.getLastName()+" - ";
         if (action >= 0 && action < actionDescription.length) {
-            entry += person.getFirstName();
-            entry += person.getLastName();
+            entry += person.getFirstName()+" ";
+            entry += person.getLastName()+" - ";
             entry += actionDescription[action];
         } else {
             entry += "unknown action";
         }
     }
 
+    /**
+     * Gives back the entry in String format.
+     * @return entry as String.
+     */
     public String getEntry() {
         return entry;
     }
