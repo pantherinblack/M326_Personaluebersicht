0024dd94f404b524f670f93f1f1da28768a21279
pantherinblack <stupark@bzz.ch>
2022-06-19 22:47:31 +0200

    BasicPerson edit

--- src/ch/bzz/view/component/BasicPerson.java
+++ src/ch/bzz/view/component/BasicPerson.java
@@ -6,6 +6,9 @@ import layout.TableLayout;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import static ch.bzz.util.ColorCodes.*;
@@ -14,12 +17,15 @@ public class BasicPerson extends JPanel implements ViewListener {
     private JLabel nameLabel = new JLabel("Name: ");
     private JTextField nameField = new JTextField("Unknown");
     private JLabel photo = new JLabel();
+    private JLabel smallPhoto = new JLabel();
+    private boolean addPhoto;
 
-    public BasicPerson(String uuid) {
-        this();
+    public BasicPerson(String uuid, boolean addPhoto) {
+        this(addPhoto);
         fireChanges(uuid);
     }
-    public BasicPerson() {
+    public BasicPerson(boolean addPhoto) {
+        this.addPhoto = addPhoto;
         initLayout();
         fireChanges(null);
         nameField.setEditable(false);
@@ -27,23 +33,33 @@ public class BasicPerson extends JPanel implements ViewListener {
     }
 
     private void initLayout() {
-        double[][] order = {{-3,-1},{-3,-1}};
+        double[][] order = {{-3,-1,-3},{-3,-1,-3}};
 
-        this.setLayout(new TableLayout(order));
+        setLayout(new TableLayout(order));
 
-        this.add(nameLabel, "0, 0, 0, 0");
-        this.add(nameField, "1, 0, 1, 0");
-        this.add(photo, "1, 1, 1, 1");
+        smallPhoto.setIcon(new ImageIcon(new ImageIcon("images/img.png").getImage().getScaledInstance(20, 20, Image.SCALE_FAST)));
+
+        add(nameLabel, "0, 0, 0, 0");
+        add(nameField, "1, 0, 1, 0");
+        add(photo, "1, 1, 1, 2");
+        if (addPhoto) {
+            add(smallPhoto, "2, 2, 2, 2");
+        }
 
         nameLabel.setBorder(FRAME_BORDER);
         nameField.setBorder(FRAME_BORDER);
         photo.setBorder(FRAME_BORDER);
-        this.setBorder(FRAME_BORDER);
+        smallPhoto.setBorder(FRAME_BORDER);
+        setBorder(FRAME_BORDER);
+    }
+
+    public void setPictureActionListener(MouseListener mouseListener) {
+        smallPhoto.addMouseListener(mouseListener);
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("test");
-        frame.add(new BasicPerson());
+        frame.add(new BasicPerson(true));
         frame.setVisible(true);
         frame.pack();
     }
