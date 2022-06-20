a5aa64a6dd960f176cf10008f79afc4f78f40b89
Marko Joksimovic <joksimovicm@bzz.ch>
2022-06-20 07:54:07 +0200

    Main-File

---
+++ src/gui/Test.java
@@ -0,0 +1,13 @@
+package gui;
+
+/**
+ * @author Marko Joksimovic
+ * @version 1.0
+ * @date
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Startseite startseite = new Startseite("I am looking for");
+    }
+}
